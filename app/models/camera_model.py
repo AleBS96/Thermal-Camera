c09279601d8f630a3d1eaf1e697058d0eb16700b
Alejandro <fealejandrob@gmail.com>
2025-01-21 12:08:03 -0600

    Se desactivo la autoconversion a rgb de opencv

--- app/models/camera_model.py
+++ app/models/camera_model.py
@@ -1,4 +1,5 @@
 import cv2
+import platform
 
 class CameraModel:
 
@@ -11,6 +12,11 @@ class CameraModel:
         
         # Configurar la cámara para que entregue frames en el formato deseado
         if(color_space != "Default"):
+            # Obtener el sistema operativo
+            os_name = platform.system()
+            if(os_name == "Linux"):
+                # Configurar para evitar la conversión automática a BGR
+                self.cap.set(cv2.CAP_PROP_CONVERT_RGB, 0)
             self.cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*color_space))
     
     def get_frame(self):
